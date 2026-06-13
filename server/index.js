bf42da8a9125a05eca2c4d5a0f7f98eff25a946a
terryzeng007 <terryzeng007@github.com>
2026-06-13 16:18:11 +0800

    Revert: restore record_type filter (user will fix strategy_4 data instead)

--- server/index.js
+++ server/index.js
@@ -168,6 +168,7 @@ app.get('/api/signals/:symbol/:strategy', async (req, res) => {
     const signals = await Model.find({
       symbol: symbol.toLowerCase(),
       strategy_id: strategy,
+      record_type: 'daily_signal',
       position_action: { $in: ['open', 'close'] }
     }).sort({ date: 1 });
     
